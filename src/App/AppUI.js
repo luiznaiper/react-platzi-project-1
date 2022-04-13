ec101bd1dca91fe61eede46f566854a6cca81ecf
luiznaiper <luiz_naiper@hotmail.com>
2022-04-13 09:55:19 -0500

    Use Context and Todo Context

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,58 +1,48 @@
 import React from "react"
-import { TodoCounter } from "../TodoCounter";
-import { TodoSearch } from "../TodoSearch/index.js";
-import { TodoList } from "../TodoList/index.js";
-import { TodoItem } from "../TodoItem/index.js";
-import { CreateTodoButton } from "../CreateTodoButton/index.js";
-import { HideCompletedTodos } from "../HideCompletedTodos";
-import { ShowCompletedTodos } from "../ShowCompletedTodos";
-import { TodoCongrats } from "../TodoCongrats";
+import { TodoContext } from "../TodoContext"
+import { TodoCounter } from "../TodoCounter"
+import { TodoSearch } from "../TodoSearch/index.js"
+import { TodoList } from "../TodoList/index.js"
+import { TodoItem } from "../TodoItem/index.js"
+import { CreateTodoButton } from "../CreateTodoButton/index.js"
+import { HideCompletedTodos } from "../HideCompletedTodos"
+import { ShowCompletedTodos } from "../ShowCompletedTodos"
+import { TodoCongrats } from "../TodoCongrats"
 
-function AppUI({
-      loading,
-      error,
-      totalTodos,
-      completedTodos,
-      searchValue,
-      setSearchValue,
-      searchedTodos,
-      toggleCompleteTodo,
+function AppUI(){
+    const {
+      error, 
+      loading, 
+      searchedTodos, 
+      toggleCompleteTodo, 
       deleteTodo,
       hideCompletedTodosFn,
       showCompletedTodosFn
-}){
+    } = React.useContext(TodoContext)
     return(
    <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />    
-      <TodoCongrats
-          total={totalTodos}
-      />
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      <TodoList>
-        { error && <p>We have an error, reload the page...</p> }
-        { loading && <p>Loading, be patient...</p> }
-        { (!loading && !searchedTodos.length) && <p> Create your first Task </p> }
-
-        {searchedTodos.map(todo =>(
-          <TodoItem 
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={()=> toggleCompleteTodo (todo.text)}
-            onDelete={()=> deleteTodo (todo.text)}
-            hide={todo.hide}
-           />
-        ))}
-      </TodoList>
+      <TodoCounter/>    
+      <TodoCongrats/>
+      <TodoSearch/>
+           <TodoList>
+           { error && <p>We have an error, reload the page...</p> }
+           { loading && <p>Loading, be patient...</p> }
+           { (!loading && !searchedTodos.length) && <p> Create your first Task </p> }
+   
+           {searchedTodos.map(todo =>(
+             <TodoItem 
+               key={todo.text}
+               text={todo.text}
+               completed={todo.completed}
+               onComplete={()=> toggleCompleteTodo (todo.text)}
+               onDelete={()=> deleteTodo (todo.text)}
+               hide={todo.hide}
+              />
+           ))}
+         </TodoList>
       <CreateTodoButton />      
       <HideCompletedTodos
-        onHide={()=> hideCompletedTodosFn()}
+       onHide={()=> hideCompletedTodosFn()}
       />      
       <ShowCompletedTodos 
         onShow={()=> showCompletedTodosFn()}
