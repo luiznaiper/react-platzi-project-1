bf03c8069b08e915b30fb48debec65fa4dd125b9
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-04-19 20:40:21 -0300

    Added icons in svg to the app

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,27 +1,24 @@
 import React from "react";
+import { CompleteIcon } from '../TodoIcon/CompleteIcon'
+import { DeleteIcon } from '../TodoIcon/DeleteIcon'
 import './TodoItem.css'
 
 function TodoItem(props) {
     
     return (
       <li className="TodoItem">
-        <span 
-          className={`Icon Icon-check ${props.completed && 'Icon-check--active'} ${props.hide && 'Icon-check--hide'}`}
-          onClick={props.onComplete}
-        >
-          √
-        </span>
+        <CompleteIcon
+          completed={props.completed}
+          onComplete={props.onComplete}
+        />
         <p 
           className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'} ${props.hide && 'TodoItem-p--hide'}`}
         >
           {props.text}
         </p>
-        <span 
-          className={`Icon Icon-delete ${props.hide && 'Icon Icon-delete--hide'}`}
-          onClick={props.onDelete}
-        >
-          X
-        </span>
+        <DeleteIcon
+          onDelete={props.onDelete}
+        />
       </li>
     );
   }
