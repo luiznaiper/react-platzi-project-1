58e6a2bcc82b08beec380c976d1b15e76ee87cec
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-04-19 23:41:40 -0300

    Updates

---
+++ precache-manifest.90e5e9a905ec7341a662f6b7047100b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ecb2b10989b0485fde",
+    "url": "./static/css/main.fdf49fe2.chunk.css"
+  },
+  {
+    "revision": "f8ecb2b10989b0485fde",
+    "url": "./static/js/main.f8ecb2b1.chunk.js"
+  },
+  {
+    "revision": "ff0b5620ef52a4c40aac",
+    "url": "./static/js/1.ff0b5620.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a155d92bc5e55a08e24dd738cf407b4a",
+    "url": "./static/media/check.a155d92b.svg"
+  },
+  {
+    "revision": "c13afa72b36fa6ff0a010d3cea719700",
+    "url": "./static/media/delete.c13afa72.svg"
+  },
+  {
+    "revision": "e7f984950efcd04df39686958aaf9806",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-platzi-project-1/precache-manifest.0f4a27e5b2123527106a2ae963e26326.js"
+  "./precache-manifest.90e5e9a905ec7341a662f6b7047100b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-platzi-project-1/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
