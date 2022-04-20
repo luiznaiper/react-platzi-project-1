4e76621e903d1be09286624acbd4ca7ee150650a
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-04-19 23:30:43 -0300

    Updates

---
+++ precache-manifest.0f4a27e5b2123527106a2ae963e26326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ecb2b10989b0485fde",
+    "url": "/react-platzi-project-1/static/css/main.fdf49fe2.chunk.css"
+  },
+  {
+    "revision": "f8ecb2b10989b0485fde",
+    "url": "/react-platzi-project-1/static/js/main.f8ecb2b1.chunk.js"
+  },
+  {
+    "revision": "ff0b5620ef52a4c40aac",
+    "url": "/react-platzi-project-1/static/js/1.ff0b5620.chunk.js"
+  },
+  {
+    "revision": "228b42555d0a41d2ba8a",
+    "url": "/react-platzi-project-1/static/js/runtime~main.228b4255.js"
+  },
+  {
+    "revision": "a155d92bc5e55a08e24dd738cf407b4a",
+    "url": "/react-platzi-project-1/static/media/check.a155d92b.svg"
+  },
+  {
+    "revision": "c13afa72b36fa6ff0a010d3cea719700",
+    "url": "/react-platzi-project-1/static/media/delete.c13afa72.svg"
+  },
+  {
+    "revision": "2212f0bd133f96c4ed221b5b9ef39660",
+    "url": "/react-platzi-project-1/index.html"
+  }
+];
