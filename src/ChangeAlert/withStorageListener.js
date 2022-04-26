a7ba0fc777c53d6bd3500f0370e8ba32c33d380d
Luis Olivárez <luiznaiper@Luiss-MacBook-Pro.local>
2022-04-26 06:43:18 -0300

    Added react. useEffect

--- src/ChangeAlert/withStorageListener.js
+++ src/ChangeAlert/withStorageListener.js
@@ -4,12 +4,20 @@ function withStorageListener(WrappedComponent){
     return function WrappedComponentWithStorageListener(props){
         const [storageChange, setStorageChange] = React.useState(false)
 
-        window.addEventListener('storage', (change)=> {
-                if(change.key === 'TODOS_V1'){
-                    console.log('changes in TODOS_V1')
-                    setStorageChange(true)
-                }
-        })
+         React.useEffect(() => {
+            const onChange = (change) => {
+              if (change.key === "TODOS_V1") {
+                console.log("Hubo cambios en TODOS_V1");
+                setStorageChange(true);
+              }
+            };
+      
+            window.addEventListener("storage", onChange);
+      
+            return () => {
+              window.removeEventListener("storage", onChange);
+            };
+          }, []);
 
         const toggleShow = ()=>{
             props.sincronize()
